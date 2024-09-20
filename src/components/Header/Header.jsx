71c360b6fa07e4de09c6f4cc88163a1820e3c5e4
PrinceRaval <divya.raval92@gmail.com>
2024-09-20 15:06:25 -0500

    new changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import logo from "../../assets/Logo.svg";
 import avatar from "../../assets/Avatar.svg";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString("default", {
@@ -13,6 +14,7 @@ function Header({ handleAddClick, weatherData }) {
       <p className="header__date-and-location">
         {currentDate},{weatherData.city}
       </p>
+      <ToggleSwitch />
       <button
         onClick={handleAddClick}
         type="button"
--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,1 +1,39 @@
+import { useState } from "react";
+import "./ToggleSwitch.css";
 
+function Toggleswitch() {
+  const [currentTempratureUnit, handleToggleSwitchChange] = useState("C");
+  const handleChange = (e) => {
+    if (currentTempratureUnit === "C") handleToggleSwitchChange("F");
+    if (currentTempratureUnit === "F") handleToggleSwitchChange("C");
+  };
+  console.log(currentTempratureUnit);
+  return (
+    <label className="switch">
+      <input type="checkbox" className="switch__box" onChange={handleChange} />
+      <span
+        className={
+          currentTempratureUnit === "F"
+            ? "switch__slider switch__slider-F"
+            : "switch__slider switch__slider-C"
+        }
+      ></span>
+      <p
+        className={`switch__temp-F ${
+          currentTempratureUnit === "F" && "switch__active"
+        }`}
+      >
+        F
+      </p>
+      <p
+        className={`switch__temp-C ${
+          currentTempratureUnit === "C" && "switch__active"
+        }`}
+      >
+        C
+      </p>
+    </label>
+  );
+}
+
+export default Toggleswitch;
