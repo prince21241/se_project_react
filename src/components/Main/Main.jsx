2208c83c89d246c3c0a8c945ce0f7bab5d32938e
PrinceRaval <divya.raval92@gmail.com>
2024-10-01 15:34:30 -0500

    new changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,35 +1,36 @@
+import React, { useContext } from "react";
 import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import { defaultClothingItems } from "../../utils/constants";
-import Itemcard from "../ItemCard/ItemCard";
+import ItemCard from "../ItemCard/ItemCard";
 import "./Main.css";
 
 function Main({ weatherData, handleCardClick }) {
   const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
-  console.log(weatherData);
+
+  // Ensure weatherData exists before accessing properties
+  if (!weatherData || !weatherData.temp || !weatherData.type) {
+    return <p>Loading weather data...</p>;
+  }
+
   return (
     <main>
       <WeatherCard weatherData={weatherData} />
       <section className="cards">
         <p className="cards__text">
-          Today is {weatherData.temp[currentTemperatureUnit]} &deg; F / You may
+          Today is {weatherData.temp[currentTemperatureUnit]} &deg; / You may
           want to wear:
         </p>
         <ul className="cards__list">
-          {" "}
           {defaultClothingItems
-            .filter((item) => {
-              return item.weather === weatherData.type;
-            })
-            .map((item) => {
-              return (
-                <Itemcard
-                  key={item._id}
-                  item={item}
-                  onCardClick={handleCardClick}
-                />
-              );
-            })}
+            .filter((item) => item.weather === weatherData.type)
+            .map((item) => (
+              <ItemCard
+                key={item._id}
+                item={item}
+                onCardClick={handleCardClick}
+              />
+            ))}
         </ul>
       </section>
     </main>
