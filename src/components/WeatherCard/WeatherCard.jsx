10bdf109c1aec8687b5dd248836c50ed89300f90
PrinceRaval <divya.raval92@gmail.com>
2024-10-01 15:55:17 -0500

    new changes

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,10 +1,17 @@
 import sunny from "../../assets/sunny.svg";
 import "./WeatherCard.css";
+import React, { useContext } from "react";
+import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 
 function WeatherCard({ weatherData }) {
+  // Use useContext to get the current temperature unit
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+
   return (
     <section className="weather-card">
-      <p className="weather-card__temp"> {weatherData.temp.F} &deg; F</p>
+      <p className="weather-card__temp">
+        {weatherData.temp[currentTemperatureUnit]}
+      </p>
       <img src={sunny} alt="Weather-Card" className="weather-card__image" />
     </section>
   );
